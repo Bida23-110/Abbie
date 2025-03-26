806feea64d6d64e89932da97fa0082b5a54d21e0
Bida23-110 <159067483+Bida23-110@users.noreply.github.com>
2025-03-26 22:03:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.getElementById('revealMessage').addEventListener('click', function() {
-    const message = document.getElementById('hiddenMessage');
-    message.style.display = 'block';
-});
+
