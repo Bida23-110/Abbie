350bca4d57d412abc71bf845e431e5630eb0eea6
Bida23-110 <159067483+Bida23-110@users.noreply.github.com>
2025-03-26 21:56:27 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('revealMessage').addEventListener('click', function() {
+    const message = document.getElementById('hiddenMessage');
+    message.style.display = 'block';
+});
